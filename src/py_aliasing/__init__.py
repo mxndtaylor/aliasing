226af4ecac931c5e2f357d58a90a2dc0d0884a87
mxt <mxnd.taylor@gmail.com>
2024-05-30 10:11:26 -0500

    fix package exports

--- src/py_aliasing/__init__.py
+++ src/py_aliasing/__init__.py
@@ -1,9 +1,12 @@
-from alias import *
-from error import *
+from .alias import *
+from .virtual_alias import *
+from .error import *
 
 __all__ = [
     "alias",
     "aliased",
+    "valiased",
+    "valiases",
     "AliasError",
     "CircularAliasError",
 ]
